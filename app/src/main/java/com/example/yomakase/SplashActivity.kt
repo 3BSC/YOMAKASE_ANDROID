c31a578713b101b54798d21ba8dc90795c5590b9
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-03-08 01:40:22 +0900

    feat: add login ui

--- app/src/main/java/com/example/yomakase/SplashActivity.kt
+++ app/src/main/java/com/example/yomakase/SplashActivity.kt
@@ -12,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
