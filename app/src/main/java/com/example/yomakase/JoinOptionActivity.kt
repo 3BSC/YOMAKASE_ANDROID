6405f18a94e913f415add37ba583ae7b06d2868e
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-03-13 22:48:06 +0900

    feat: add join option ui

---
+++ app/src/main/java/com/example/yomakase/JoinOptionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yomakase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinOptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_option)
+    }
+}
