1e3516da507573524b3dd9c322e1af0a74e631b7
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-03-13 23:14:59 +0900

    feat: add JoinGeneralMemberActivity

---
+++ app/src/main/java/com/example/yomakase/JoinGeneralMemberActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yomakase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinGeneralMemberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_general_member)
+    }
+}
