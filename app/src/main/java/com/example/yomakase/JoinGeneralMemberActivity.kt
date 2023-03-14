510a2648164d727beb4f20d5171eaa9ac2833de5
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-03-15 00:11:07 +0900

    feat: attach spinner on ui

--- app/src/main/java/com/example/yomakase/JoinGeneralMemberActivity.kt
+++ app/src/main/java/com/example/yomakase/JoinGeneralMemberActivity.kt
@@ -2,10 +2,37 @@ package com.example.yomakase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import com.example.yomakase.databinding.ActivityJoinGeneralMemberBinding
+import java.text.DateFormatSymbols
 
 class JoinGeneralMemberActivity : AppCompatActivity() {
+    lateinit var binding: ActivityJoinGeneralMemberBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join_general_member)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_join_general_member)
+        setupSpinnerEmailDomain()
+        setupSpinnerHandler()
+    }
+
+    private fun setupSpinnerEmailDomain() {
+        val domains = resources.getStringArray(R.array.sp_email_domain)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, domains)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spEmailDomain.adapter = adapter
+    }
+
+    private fun setupSpinnerHandler() {
+        binding.spEmailDomain.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Log.d("email", "onItemSelected: ${binding.spEmailDomain.getItemAtPosition(position)}")
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
     }
 }
