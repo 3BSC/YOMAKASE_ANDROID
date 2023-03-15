313324acf954ad48a90ae04af591c8bc746068a7
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-03-15 23:21:13 +0900

    feat: change birth spinner to datepicker and add passwordcheck hide/ show button

--- app/src/main/java/com/example/yomakase/JoinGeneralMemberActivity.kt
+++ app/src/main/java/com/example/yomakase/JoinGeneralMemberActivity.kt
@@ -1,22 +1,35 @@
 package com.example.yomakase
 
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.res.Resources.Theme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.yomakase.databinding.ActivityJoinGeneralMemberBinding
+import com.google.android.material.materialswitch.MaterialSwitch
 import java.text.DateFormatSymbols
+import java.text.SimpleDateFormat
+import java.util.*
 
 class JoinGeneralMemberActivity : AppCompatActivity() {
     lateinit var binding: ActivityJoinGeneralMemberBinding
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+    private var pwVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_join_general_member)
         setupSpinnerEmailDomain()
         setupSpinnerHandler()
+        setTodayDate()
     }
 
     private fun setupSpinnerEmailDomain() {
@@ -34,5 +47,35 @@ class JoinGeneralMemberActivity : AppCompatActivity() {
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
         }
+    }
+
+    private fun setTodayDate(){
+        binding.tvBirth.text = dateFormat.format(Date(System.currentTimeMillis()))
+    }
+
+    fun dataPicker(view: View){
+        val cal = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            binding.tvBirth.text = "$year-${month+1}-$dayOfMonth"
+        }
+        DatePickerDialog(this, android.R.style.Theme_Holo_Light_Dialog_NoActionBar
+            ,dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+    }
+
+    fun visiblePassword(view: View){
+        when(pwVisible){
+          false -> {
+              pwVisible = true
+              binding.etPasswordCheck.transformationMethod = HideReturnsTransformationMethod.getInstance()
+              binding.btnPasswordHide.setImageResource(R.drawable.ic_hide)
+          }
+          true -> {
+              pwVisible = false
+              binding.etPasswordCheck.transformationMethod = PasswordTransformationMethod.getInstance()
+              binding.btnPasswordHide.setImageResource(R.drawable.ic_show)
+          }
+        }
+        binding.etPasswordCheck.setTextAppearance(R.style.join_text)
+        binding.etPasswordCheck.setSelection(binding.etPasswordCheck.text!!.length)
     }
 }
