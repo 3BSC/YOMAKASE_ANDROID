6969cc23804bad2b21fa3f0fe15ea25dc3d00cae
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-03-16 00:22:54 +0900

    feat: add JoinOwnerMemberActivity and wrap input layout with scrollview

---
+++ app/src/main/java/com/example/yomakase/JoinOwnerMemberActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yomakase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinOwnerMemberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join_owner_member)
+    }
+}
