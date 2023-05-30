0d2ae0483cd317b23ec944822ed5ee505ad4a538
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-05-30 17:30:59 +0900

    feat: apply databinding in ProfileGeneralFragment

--- app/src/main/java/com/example/yomakase/view/ProfileGeneralFragment.kt
+++ app/src/main/java/com/example/yomakase/view/ProfileGeneralFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.yomakase.R
+import com.example.yomakase.databinding.FragmentProfileGeneralBinding
 
 
 class ProfileGeneralFragment : Fragment() {
+    private lateinit var binding: FragmentProfileGeneralBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_general, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_general, container, false)
+        return binding.root
     }
 
 }
