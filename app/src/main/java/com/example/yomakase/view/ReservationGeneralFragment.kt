70e4f3052e06a2043dab66fa43b4b7ce2d637269
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-05-30 17:29:37 +0900

    feat: apply databinding in ReservationGeneralFragment

--- app/src/main/java/com/example/yomakase/view/ReservationGeneralFragment.kt
+++ app/src/main/java/com/example/yomakase/view/ReservationGeneralFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.yomakase.R
+import com.example.yomakase.databinding.FragmentReservationGeneralBinding
 
 
 class ReservationGeneralFragment : Fragment() {
 
+    private lateinit var binding: FragmentReservationGeneralBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reservation_general, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_reservation_general, container, false)
+
+        return binding.root
     }
-
-
 }
