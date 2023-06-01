296e6bbb88244ac8e53abba20de20cf1922ef928
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-06-01 16:47:51 +0900

    feat: add bottom navigation of owner account

---
+++ app/src/main/java/com/example/yomakase/view/Owner/OwnerManageStoreFragment.kt
@@ -0,0 +1,17 @@
+package com.example.yomakase.view.Owner
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.yomakase.R
+
+class OwnerManageStoreFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_owner_manage_store, container, false)
+    }
+}
